56f8105cbc69b529f537eca0907b0a09844d5a57
Ruth Delgado <ruthydd@gmail.com>
2020-06-18 15:38:27 +0200

    change in function name

---
+++ R/setDBfile.R
@@ -0,0 +1,19 @@
+#' Set the LFMC database file
+#'
+#' Specifies the location of the LFMC database file. This causes setting global variable \code{lfmcdbfile}.
+#'
+#' @param file A string with the path to a *.sqlite file
+#'
+#' @examples
+#' \dontrun{
+#'    set_DBpath("../lfmc.sqlite")
+#' }
+setDBpath <- function(file) {
+  if(!endsWith(file, ".sqlite")) file = paste0(file, ".sqlite")
+  if(file.exists(file)) {
+    assign("lfmcdbfile", normalizePath(file), envir = .GlobalEnv)
+    cat(paste0("Database file path set to '", lfmcdbfile, "'.\n"))
+  } else {
+    stop(paste0("Database file '", file, "' does not exist."))
+  }
+}
---
+++ R/viewDBtable.R
@@ -0,0 +1,15 @@
+extractDBtable <- function(tablename = "lfmc") {
+  lfmc_db <- DBI::dbConnect(RSQLite::SQLite(), get("lfmcdbfile"))
+  table = DBI::dbReadTable(lfmc_db, tablename)
+  if(tablename=="lfmc") {
+    table$Date = as.Date(table$Date, origin = "1970-01-01")
+  }
+  DBI::dbDisconnect(lfmc_db)
+  return(table)
+}
+
+viewDBtable <- function(tablename = "lfmc") {
+  View(extractDBtable(tablename), tablename)
+}
+
+
