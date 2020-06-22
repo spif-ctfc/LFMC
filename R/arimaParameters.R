9fa0b2e490b4e0d77835f8d9657bac76a23df7d8
Ruth Delgado <ruthydd@gmail.com>
2020-06-22 11:33:23 +0200

    arima parameters data documentation

---
+++ R/arimaParameters.R
@@ -0,0 +1,23 @@
+#' ARIMA parameters
+#'
+#' The dataset contains parameters required for ARIMA definition in the outlier search routine.
+#' These parameters were obtained by fitting an ARIMA for each site-species series,
+#' using auto.arima function from forecast package.
+#' Data includes parameters for Auto Regressive (AR) and Moving Average (MA) model terms, and seasonal component.
+#'
+#' @format A data frame with 42 rows and 10 columns
+#' \describe {
+#' \item {SamplingSiteCode}
+#' \item {SpeciesCode}
+#' \item {SiteSpCode}
+#' \item {p} {non-seasonal AR order}
+#' \item {d} {non-seasonal differencing, defined as d = 0}
+#' \item {q} {non-seasonal MA order}
+#' \item {sP} { seasonal AR order}
+#' \item {sD} {seasonal differencing}
+#' \item {sQ} {seasonal MA order}
+#' \item {Method} {fitting method used in auto.arima function}
+#' }
+#'
+#'
+"arimaParameters"
