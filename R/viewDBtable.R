7573e517798137614ff05071e17417325762d315
Ruth Delgado <ruthydd@gmail.com>
2020-08-05 14:32:49 +0200

    updated viewDBtable function

--- R/viewDBtable.R
+++ R/viewDBtable.R
@@ -1,7 +1,7 @@
 extractDBtable <- function(tablename = "lfmc") {
   lfmc_db <- DBI::dbConnect(RSQLite::SQLite(), get("lfmcdbfile"))
   table = DBI::dbReadTable(lfmc_db, tablename)
-  if(tablename=="lfmc") {
+  if(tablename == "lfmc") {
     table$Date = as.Date(table$Date, origin = "1970-01-01")
   }
   DBI::dbDisconnect(lfmc_db)
