fb5f8f7568c6742b0cff719e9559e86d4e4a7fc3
redd1326 <ruthydd@gmail.com>
2021-04-09 14:52:14 +0200

    export_slsx updated

--- R/export_xlsx.R
+++ R/export_xlsx.R
@@ -10,21 +10,113 @@
 #' \dontrun{
 #'   export_xlsx("../lfmc_backup.xlsx", overwrite = T)
 #' }
-export_xlsx<-function(xlsxfile, overwrite = FALSE) {
+#'
+export_xlsx <- function(xlsxfile, overwrite = FALSE) {
   if(!endsWith(xlsxfile, ".xlsx")) xlsxfile = paste0(xlsxfile, ".xlsx")
 
   book <- createWorkbook()
-  addWorksheet(wb = book, sheetName = "plots", gridLines = FALSE)
-  writeDataTable(wb = book, sheet = "plots", x = extract_DBtable("plots"), rowNames = TRUE, tableStyle = "none", withFilter=F)
-  addWorksheet(wb = book, sheetName = "sites", gridLines = FALSE)
-  writeDataTable(wb = book, sheet = "sites", x = extract_DBtable("sites"), rowNames = TRUE, tableStyle = "none", withFilter=F)
-  addWorksheet(wb = book, sheetName = "agents", gridLines = FALSE)
-  writeDataTable(wb = book, sheet = "agents", x = extract_DBtable("agents"), rowNames = TRUE, tableStyle = "none", withFilter=F)
-  addWorksheet(wb = book, sheetName = "species", gridLines = FALSE)
-  writeDataTable(wb = book, sheet = "species", x = extract_DBtable("species"), rowNames = TRUE, tableStyle = "none", withFilter=F)
-  addWorksheet(wb = book, sheetName = "lfmc", gridLines = FALSE)
-  writeDataTable(wb = book, sheet = "lfmc", x = extract_DBtable("lfmc"), rowNames = TRUE, tableStyle = "none", withFilter=F)
-  addWorksheet(wb = book, sheetName = "soilmoisture", gridLines = FALSE)
-  writeDataTable(wb = book, sheet = "soilmoisture", x = extract_DBtable("soilmoisture"), rowNames = TRUE, tableStyle = "none", withFilter=F)
+
+  phenology = extractDBtable("phenology")
+  if(nrow(phenology) != 0) {
+    addWorksheet(
+      wb = book,
+      sheetName = "phenology",
+      gridLines = FALSE
+      )
+    writeDataTable(
+      wb = book,
+      sheet = "phenology",
+      x = phenology,
+      rowNames = TRUE,
+      tableStyle = "none",
+      withFilter = F
+      )
+  }
+
+  lfmc = extractDBtable("lfmc")
+  if(nrow(lfmc) != 0) {
+    addWorksheet(
+      wb = book,
+      sheetName = "lfmc",
+      gridLines = FALSE
+    )
+    writeDataTable(
+      wb = book,
+      sheet = "lfmc",
+      x = lfmc,
+      rowNames = TRUE,
+      tableStyle = "none",
+      withFilter = F
+    )
+  }
+
+  species = extractDBtable("species")
+  if(nrow(species) != 0) {
+    addWorksheet(
+      wb = book,
+      sheetName = "species",
+      gridLines = FALSE
+    )
+    writeDataTable(
+      wb = book,
+      sheet = "species",
+      x = species,
+      rowNames = TRUE,
+      tableStyle = "none",
+      withFilter = F
+    )
+  }
+
+  sites = extractDBtable("sites")
+  if(nrow(sites) != 0) {
+    addWorksheet(
+      wb = book,
+      sheetName = "sites",
+      gridLines = FALSE
+    )
+    writeDataTable(
+      wb = book,
+      sheet = "sites",
+      x = sites,
+      rowNames = TRUE,
+      tableStyle = "none",
+      withFilter = F
+    )
+  }
+
+  tdr = extractDBtable("tdr_sensor")
+  if(nrow(tdr) != 0) {
+    addWorksheet(
+      wb = book,
+      sheetName = "tdr",
+      gridLines = FALSE
+    )
+    writeDataTable(
+      wb = book,
+      sheet = "tdr",
+      x = tdr,
+      rowNames = TRUE,
+      tableStyle = "none",
+      withFilter = F
+    )
+  }
+
+  soil = extractDBtable("soil_measurements")
+  if(nrow(soil) != 0) {
+    addWorksheet(
+      wb = book,
+      sheetName = "soil",
+      gridLines = FALSE
+    )
+    writeDataTable(
+      wb = book,
+      sheet = "soil",
+      x = soil,
+      rowNames = TRUE,
+      tableStyle = "none",
+      withFilter = F
+    )
+  }
+
   saveWorkbook(book, xlsxfile, overwrite = overwrite)
 }
